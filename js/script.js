82d69c15e2f7c3b5255e4c37d94c9f823f06d1b9
dpinchuk <pinchuk.dap@gmail.com>
2018-09-10 15:11:58 +0300

    homework js 11

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/*
+ Реализовать функцию, которая будет добавлять список, введенный пользователем, на страницу.
+
+ Технические требования:
+ - При запуске программы спросить у пользователя в модальном окне число пунктов, которые будут в списке.
+ - После введения пользователем числа, вывести модальное окно еще нужное количество раз (такое количество раз, какое число ввел пользователь в первом модальном окне), спрашивая у пользователя содержимое пунктов списка (пользователь может ввести любой текст).
+ - После ввода последнего значения, вывести на странице список, введенный пользователем с помощью модальных окон.
+ - Через 10 секунд очистить страницу.
+ - Необходимо использовать шаблонные строки и функцию map массива для формирования контента списка пеерд выведением его на страницу.
+*/
+
+let id = 'ulList';
+let list = document.getElementById(id);
+let arrList = [];
+let count = prompt("Input count of list items: ");
+let time = 10000;
+
+let timer = () => {
+    let elem = document.getElementById('timer');
+    elem.innerHTML--;
+    if (Number(elem.innerHTML) === 0) {
+        document.body.innerHTML = "";
+    } else {
+        setTimeout(timer, 1000);
+    }
+
+};
+
+let addLitst = (item) => {
+    let listItem = document.createElement('li');
+    listItem.innerHTML += `the current list item '${item}' is displayed on the page`;
+    list.appendChild(listItem);
+};
+
+for (let i = 0; i < count; i++) {
+    arrList[i] = prompt("Input data for [" + i + "] item of list: ")
+}
+
+arrList.map(addLitst);
+setTimeout(timer, 0);
